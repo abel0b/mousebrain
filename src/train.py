428328902970668dc5ed9b264943bab5dddcce30
Abel <abelcd@protonmail.com>
2020-02-10 19:40:22 +0100

    third epoch

--- src/train.py
+++ src/train.py
@@ -4,9 +4,14 @@ import network
 import matplotlib.pyplot
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import load_model
-from metrics import dice
 from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
+parser.add_argument("--load-model", help="Load a model")
+args = parser.parse_args()
 
 epochs = 10
 size_train = 128
@@ -17,10 +22,21 @@ data_train, data_test, label_train, label_test = data.load_data()
 train_sequence = data.MouseBrainSequence((data_train, label_train), size=size_train, batch_size=batch_size)
 data_test, label_test = data.fill_augment(data_test, label_test, batch_size)
 
-model = network.unet_model_3d((128,128,128,1), depth=2)
+
+if args.load_model:
+    model_file = args.load_model
+    dependencies = {
+        "dice_coefficient": network.dice_coefficient,
+        "dice_coefficient_loss": network.dice_coefficient
+    }
+    model = load_model(model_file, dependencies=dependencies)
+else:
+    model_file = "models/unet3d-{}".format(int(time.time()))
+    model = network.unet_model_3d((128,128,128,1), depth=2)
+
 model.summary()
 
-model_file = "models/unet3d-{}".format(int(time.time()))
+
 checkpoint = ModelCheckpoint("{}.h5".format(model_file), save_best_only=True)
 csv_logger = CSVLogger("{}-history.csv".format(model_file), append=False, separator=" ")
 
